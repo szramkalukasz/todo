c1baef99e5fb8470527bd8b7070a9084ed8bb2ee
szramkalukasz <lukasz_szramka@wp.pl>
2023-12-10 16:58:27 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import styles from "./App.module.css";
+import { Form } from "./components/Form/Form";
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div>
+          <h1>Do zrobienia</h1>
+          <h2>5 zadań</h2>
+        </div>
+        <button className={styles.button}>+</button>
+      </header>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,11 @@
+import { Button } from "./Button/Button";
+import styles from "./Form.module.css";
+
+export function Form() {
+  return (
+    <form className={styles.form}>
+      <input className={styles.input} type="text" />
+      <Button>Dodaj</Button>
+    </form>
+  );
+}
